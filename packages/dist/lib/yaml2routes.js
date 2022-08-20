ad669349371a014c8af936a1693509d140e04374
TomokaKobayashi <chandla@bc.mbn.or.jp>
2022-08-21 01:23:41 +0900

    feat: implement yaml2routes tool.

--- packages/dist/lib/yaml2routes.js
+++ packages/dist/lib/yaml2routes.js
@@ -4,25 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const make_endpoints_1 = require("./make-endpoints");
 const commander_1 = __importDefault(require("commander"));
 commander_1.default
     .version("0.0.1", "-v --version")
     .usage("[options]")
     .option("-i --input <fileName | dirName>", "input file or directory name(required)")
+    .option("-o --output <fileName>", "output file name for routes.json(default: 'routes.json'")
     .option("-p --prefix <prefix>", "response data file prefix")
-    .option("-v --with-validation", "enable to output validation parameters")
+    .option("-s --stereo-type <fileName", "prototype of output routes.jsib")
+    .option("-w --with-validation", "enable to output validation parameters")
     .parse(process.argv);
 const options = commander_1.default.opts();
 const input = options.input;
 const prefix = options.prefix;
+const output = options.output || 'routes.json';
 const withValidation = options.withValidation;
+const stereoTypeFile = options.stereoType;
 if (!input) {
-    console.log('ERROR: input is required.');
+    console.error('ERROR: input is required.');
     process.exit(1);
 }
 if (!fs_1.default.existsSync(input)) {
-    console.log('ERROR: input does not exist.');
+    console.error('ERROR: input does not exist.');
     process.exit(1);
 }
 // input is file or dir?
@@ -54,17 +59,90 @@ else {
     // only single file
     targets.push(input);
 }
-// read and process yaml(s)
-const endpoints = [];
-for (const target of targets) {
+// read stereo type
+let stereoType = {};
+if (stereoTypeFile) {
+    if (!fs_1.default.existsSync(stereoTypeFile)) {
+        console.error('ERROR: stereoType does not exist.');
+        process.exit(1);
+    }
+    const stat = fs_1.default.statSync(stereoTypeFile);
+    if (!stat.isFile()) {
+        console.error('ERROR: stereoType is not file.');
+        process.exit(1);
+    }
     try {
-        const content = fs_1.default.readFileSync(target, 'utf-8');
-        const endpoint = (0, make_endpoints_1.makeEndpointsFromYaml)(content, target);
-        endpoints.push(...endpoint);
+        stereoType = JSON.parse(fs_1.default.readFileSync(stereoTypeFile, 'utf-8'));
     }
     catch (err) {
-        console.error(`ERROR: file:'${target} can not be processed.`);
+        console.error('ERROR: can\'t read stereoType.');
         console.error(err);
+        process.exit(1);
     }
 }
-// output routes.json
+// make outputPath
+const dirName = path_1.default.dirname(output);
+try {
+    // read and process yaml(s)
+    const endpoints = [];
+    for (const target of targets) {
+        try {
+            const content = fs_1.default.readFileSync(target, 'utf-8');
+            const endpoint = (0, make_endpoints_1.makeEndpointsFromYaml)(content, target);
+            endpoints.push(...endpoint);
+        }
+        catch (err) {
+            console.error(`ERROR: file:'${target} can not be processed.`);
+            console.error(err);
+        }
+    }
+    // if not need validation, delete validation infos.
+    if (!withValidation) {
+        for (const endpoint of endpoints) {
+            endpoint.validatorArgs = undefined;
+        }
+    }
+    // if prefix option exist, write data files to individual files
+    if (prefix) {
+        let fileIndex = 1;
+        endpoints.forEach((endpoint) => {
+            endpoint.matches.forEach(match => {
+                if (match.metadataType === 'immediate') {
+                    const metaData = match.metadata;
+                    const data = metaData.data;
+                    if (data) {
+                        const fileName = prefix + fileIndex;
+                        const fullPath = path_1.default.resolve(dirName, fileName);
+                        fileIndex++;
+                        if (metaData.datatype == 'object') {
+                            const outputData = JSON.stringify(data, null, '  ');
+                            fs_1.default.writeFileSync(fullPath, outputData);
+                            metaData.datatype = 'file';
+                            metaData.data = fileName;
+                        }
+                        else if (metaData.datatype == 'value') {
+                            const value = data;
+                            try {
+                                const jsonData = JSON.parse(value);
+                                fs_1.default.writeFileSync(fullPath, JSON.stringify(jsonData, null, '  '));
+                            }
+                            catch (err) {
+                                fs_1.default.writeFileSync(fullPath, value);
+                            }
+                            metaData.datatype = 'file';
+                            metaData.data = fileName;
+                        }
+                    }
+                }
+            });
+        });
+    }
+    // output routes.json
+    const outputData = Object.assign(Object.assign({}, stereoType), { endpoints });
+    fs_1.default.writeFileSync(path_1.default.resolve(dirName, output), JSON.stringify(outputData, null, '  '));
+}
+catch (error) {
+    console.error('ERROR: some error occurred!');
+    console.log(error);
+    process.exit(1);
+}
--- packages/server/src/lib/yaml2routes.ts
+++ packages/server/src/lib/yaml2routes.ts
@@ -1,27 +1,32 @@
 import fs from 'fs';
+import path from 'path';
 import { makeEndpointsFromYaml } from './make-endpoints';
 import commander from "commander";
-import { Endpoint } from './types';
+import { Endpoint, Metadata } from './types';
 
 commander
   .version("0.0.1", "-v --version")
   .usage("[options]")
   .option("-i --input <fileName | dirName>", "input file or directory name(required)")
+  .option("-o --output <fileName>", "output file name for routes.json(default: 'routes.json'")
   .option("-p --prefix <prefix>", "response data file prefix")
-  .option("-v --with-validation", "enable to output validation parameters")
+  .option("-s --stereo-type <fileName", "prototype of output routes.jsib")
+  .option("-w --with-validation", "enable to output validation parameters")
   .parse(process.argv);
 
 const options = commander.opts();
 const input = options.input as string;
 const prefix = options.prefix as string;
+const output = options.output as string || 'routes.json';
 const withValidation = options.withValidation as boolean;
+const stereoTypeFile = options.stereoType;
 
 if(!input){
-  console.log('ERROR: input is required.');
+  console.error('ERROR: input is required.');
   process.exit(1);
 }
 if(!fs.existsSync(input)){
-  console.log('ERROR: input does not exist.');
+  console.error('ERROR: input does not exist.');
   process.exit(1);
 }
 // input is file or dir?
@@ -53,17 +58,89 @@ if(dirFlag){
   targets.push(input);
 }
 
-// read and process yaml(s)
-const endpoints: Endpoint[] = [];
-for(const target of targets){
+// read stereo type
+let stereoType = {};
+if(stereoTypeFile){
+  if(!fs.existsSync(stereoTypeFile)){
+    console.error('ERROR: stereoType does not exist.');
+    process.exit(1);
+  }
+  const stat = fs.statSync(stereoTypeFile);
+  if(!stat.isFile()){
+    console.error('ERROR: stereoType is not file.');
+    process.exit(1);
+  }
   try{
-    const content = fs.readFileSync(target, 'utf-8');
-    const endpoint = makeEndpointsFromYaml(content, target);
-    endpoints.push(...endpoint);
+    stereoType = JSON.parse(fs.readFileSync(stereoTypeFile, 'utf-8'));
   }catch(err){
-    console.error(`ERROR: file:'${target} can not be processed.`);
+    console.error('ERROR: can\'t read stereoType.');
     console.error(err);
+    process.exit(1);
   }
 }
+// make outputPath
+const dirName = path.dirname(output);
 
-// output routes.json
+try{
+  // read and process yaml(s)
+  const endpoints: Endpoint[] = [];
+  for(const target of targets){
+    try{
+      const content = fs.readFileSync(target, 'utf-8');
+      const endpoint = makeEndpointsFromYaml(content, target);
+      endpoints.push(...endpoint);
+    }catch(err){
+      console.error(`ERROR: file:'${target} can not be processed.`);
+      console.error(err);
+    }
+  }
+
+  // if not need validation, delete validation infos.
+  if(!withValidation){
+    for(const endpoint of endpoints){
+      endpoint.validatorArgs = undefined;
+    }
+  }
+
+  // if prefix option exist, write data files to individual files
+  if(prefix){
+    let fileIndex = 1;
+    endpoints.forEach((endpoint)=>{
+      endpoint.matches.forEach(match=>{
+        if(match.metadataType ==='immediate'){
+          const metaData = match.metadata as Metadata;
+          const data = metaData.data;
+          if(data){
+            const fileName = prefix + fileIndex;
+            const fullPath = path.resolve(dirName, fileName);
+            fileIndex ++;
+            if(metaData.datatype=='object'){
+              const outputData = JSON.stringify(data, null, '  ');
+              fs.writeFileSync(fullPath, outputData);
+              metaData.datatype = 'file';
+              metaData.data = fileName;
+            }else if(metaData.datatype=='value'){
+              const value = data as string;
+              try{
+                const jsonData = JSON.parse(value)
+                fs.writeFileSync(fullPath, JSON.stringify(jsonData, null, '  '));
+              }catch(err){
+                fs.writeFileSync(fullPath, value);
+              }
+              metaData.datatype = 'file';
+              metaData.data = fileName;
+            }
+          }
+        }
+      });
+    });
+  }
+
+  // output routes.json
+  const outputData = {...stereoType, endpoints};
+  fs.writeFileSync(path.resolve(dirName, output), JSON.stringify(outputData, null, '  '));
+}catch(error){
+  console.error('ERROR: some error occurred!');
+  console.log(error);
+  process.exit(1);
+}
