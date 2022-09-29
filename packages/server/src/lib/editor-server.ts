a8f090dd6cfdcbceab861a7bb73a5c40a0bd24cf
TomokaKobayashi <chandla@bc.mbn.or.jp>
2022-09-30 00:41:21 +0900

    working: define data types.

--- packages/server/src/lib/editor-server.ts
+++ packages/server/src/lib/editor-server.ts
@@ -1,8 +1,50 @@
 import express from "express";
 import commander from "commander";
 import path from "path";
+import { DataType, Endpoint, Metadata, Pattern, Routes } from "common";
 
-//
+// model of data
+type DataModel = {
+  path: string
+  dataType: DataType
+  rawData: string
+  objectData: any
+};
+
+// model of metadata
+type MetadataModel = Omit<Metadata, "data" | "datatype"> & {
+  data: DataModel
+};
+
+// model of match
+type MatchModel = Omit<Pattern, "metadata"> & {
+  metadata: MetadataModel
+};
+
+// model of endpoint
+type EndpointModel = Omit<Endpoint, "matches"> &
+{
+  matches: MatchModel[]
+};
+
+// model of endpoints.json
+type EndpointsModel = {
+  path: string
+  endpoints: EndpointModel[]
+};
+
+// model of routes.json
+type RoutesModel = Omit<Routes, "endpoints"> &
+{
+  endpoints: EndpointsModel[]
+}
+
+
+
+// read routes.json
+const readRoutes = (fileName: string) => {
+
+};
 
 // parsing parameters.
 commander
@@ -19,6 +61,11 @@ if (!routes) {
   console.error("ERROR!!! : -r or --routes is required.");
   process.exit(1);
 }
+
+// initialze this app.
+
+
+
 
 // create app
 const app = express();
